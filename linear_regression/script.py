5c6696328f2170ed18213859fe175e5e0923f412
Csillag61 <csillagok@gmail.com>
2022-04-14 15:41:06 -0400

    OLS estimation

--- linear_regression/script.py
+++ linear_regression/script.py
@@ -123,5 +123,93 @@ test_rmse = np.sqrt(test_mse)
 print(train_rmse)
 print(test_rmse)
 
+# gradient descent algorithm works by iteratively trying different parameter values until the model with the lowest mean squared error is found
+
+def derivative(a1, xi_list, yi_list):
+    len_data = len(xi_list)
+    error = 0
+    for i in range(0, len_data):
+        error += xi_list[i]*(a1*xi_list[i] - yi_list[i])
+    deriv = 2*error/len_data
+    return deriv
 
 
+def gradient_descent(xi_list, yi_list, max_iterations, alpha, a1_initial):
+    a1_list = [a1_initial]
+
+    for i in range(0, max_iterations):
+        a1 = a1_list[i]
+        deriv = derivative(a1, xi_list, yi_list)
+        a1_new = a1 - alpha*deriv
+        a1_list.append(a1_new)
+    return(a1_list)
+
+param_iterations = gradient_descent(train['Gr Liv Area'], train['SalePrice'], 20, .0000003, 150)
+final_param = param_iterations[-1]
+
+def a1_derivative(a0, a1, xi_list, yi_list):
+    len_data = len(xi_list)
+    error = 0
+    for i in range(0, len_data):
+        error += xi_list[i]*(a0 + a1*xi_list[i] - yi_list[i])
+    deriv = 2*error/len_data
+    return deriv
+
+def a0_derivative(a0, a1, xi_list, yi_list):
+    len_data=len(xi_list)
+    error=0
+    for i in range(0, len_data):
+        error += a0 + a1*xi_list[i] - yi_list[i]
+    deriv = 2*error/len_data
+    return deriv
+    return 1
+
+def gradient_descent(xi_list, yi_list, max_iterations, alpha, a1_initial, a0_initial):
+    a1_list = [a1_initial]
+    a0_list = [a0_initial]
+
+    for i in range(0, max_iterations):
+        a1 = a1_list[i]
+        a0 = a0_list[i]
+        
+        a1_deriv = a1_derivative(a0, a1, xi_list, yi_list)
+        a0_deriv = a0_derivative(a0, a1, xi_list, yi_list)
+        
+        a1_new = a1 - alpha*a1_deriv
+        a0_new = a0 - alpha*a0_deriv
+        
+        a1_list.append(a1_new)
+        a0_list.append(a0_new)
+    return(a0_list, a1_list)
+
+# Uncomment when ready.
+a0_params, a1_params = gradient_descent(train['Gr Liv Area'], train['SalePrice'], 20, .0000003, 150, 1000)
+
+#  OLS estimation/ Ordinary Least Squares
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('AmesHousing.txt', delimiter="\t")
+train = data[0:1460]
+test = data[1460:]
+
+features = ['Wood Deck SF', 'Fireplaces', 'Full Bath', '1st Flr SF', 'Garage Area',
+       'Gr Liv Area', 'Overall Qual']
+
+X = train[features]
+X['bias']= 1
+X=X[['bias'] + features]
+y= train['SalePrice']
+first_term = np.linalg.inv(
+        np.dot(
+            np.transpose(X), 
+            X
+        )
+    )
+second_term = np.dot(
+        np.transpose(X),
+        y
+    )
+ols_estimation = np.dot(first_term, second_term)
+print(ols_estimation)
